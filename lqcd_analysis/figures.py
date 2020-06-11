f6e246e116831f99ac45b7d474e137b998daa481
William Jay <wjay@fnal.gov>
2020-06-11 16:33:58 -0500

    Refactor plot_states(...)

--- lqcd_analysis/figures.py
+++ lqcd_analysis/figures.py
@@ -1,5 +1,8 @@
 import numpy as np
+from collections import namedtuple
+import seaborn as sns
 from . import visualize as plt
+
 
 def plot_form_factor_results(form_factor_fit, axarr=None):
     """
@@ -24,47 +27,15 @@ def plot_form_factor_results(form_factor_fit, axarr=None):
     return fig, axarr
 
 
-def _plot_fit_energies(ax, form_factor_fit, energy_tag, with_priors=True):
-    """Plots the fit energies associated with 'energy_tag'."""
-    # Plot the fit spectrum
-    masses = np.cumsum(form_factor_fit.fits['full'].p[energy_tag])
-    colors = plt.color_palette(n_colors=len(masses))
-    for idx, mass in enumerate(masses):
-        plt.axhline(ax, mass, label=f'Fit: E{idx}',
-                        alpha=0.75, color=colors[idx])
-    # Overlay the priors
-    if with_priors:
-        masses = np.cumsum(form_factor_fit.fits['full'].prior[energy_tag])
-        print(colors, masses)
-        for idx, mass in enumerate(masses):
-            plt.axhspan(ax, mass, label=f'Prior: E{idx}',
-                            alpha=0.25, color=colors[idx])
-    return ax
-
 def _plot_meff(ax, corr):
-    """Plots the effective mass and the FastFit guess for the given tag."""
+    """
+    Plots the effective mass and the FastFit guess for the given tag.
+    """
     ax = corr.plot_meff(ax=ax, avg=False, fmt='.', label='Effective mass')
     ax = corr.plot_meff(ax=ax, avg=True, fmt='.',label='Smeared effective mass')
     plt.axhline(ax, corr.fastfit.E, color='k', ls=':', label='FastFit guess')
     return ax
 
-
-def plot_energy_summary(ax, form_factor_fit, tag, osc=False, with_priors=True):
-    """ Makes summary plot of the energies."""
-    if osc:
-        energy_tag = f'{tag}:dEo'
-        title = f'Energies (oscillating states): {tag}'
-    else:
-        energy_tag = f'{tag}:dE'
-        title = f'Energies: {tag}'
-        # Effective mass defined for decaying states only
-        _plot_meff(ax, form_factor_fit.ds[tag])
-    _plot_fit_energies(ax, form_factor_fit, energy_tag, with_priors)
-    ax.set_title(title)
-    ax.set_xlabel("$t/a$")
-    ax.set_ylabel("$Ea$")
-    ax.legend(loc=1)
-    return ax
 
 def _plot_amp_eff(ax, corr):
     """
@@ -77,75 +48,48 @@ def _plot_amp_eff(ax, corr):
     meff = corr.meff(avg=True)
     t = corr.times.tdata[1:-1]
     y = np.sqrt((np.exp(meff * t)) * corr.avg()[1:-1])
+    y = y * np.sqrt(2.0 * corr.fastfit.E)
     # stop around halfway, since we neglect backward propagation
     tmax = min(corr.times.nt // 2, max(t))
     plt.errorbar(ax, t[:tmax], y[:tmax],
                         fmt='.', label='Effective amplitude')
     # Fastfit guess
-    amp_ffit = np.sqrt(corr.fastfit.ampl)
+    amp_ffit = np.sqrt(corr.fastfit.ampl) * np.sqrt(2.0 * corr.fastfit.E)
     plt.axhline(ax, amp_ffit, label='ffit guess', color='k', ls=':')
     return ax
 
-def _plot_fit_amplitudes(ax, form_factor_fit, amp_tag, with_priors=True):
-    """Plots the fit energies associated with 'energy_tag'."""
-    # Plot the fit amplitudes
-    amps = form_factor_fit.fits['full'].p[amp_tag]
-    colors = plt.color_palette(n_colors=len(amps))
-    for idx, amp in enumerate(amps):
-        label = f'Fit: A{idx}'
-        plt.axhline(ax, amp, label=label, color=colors[idx])
-    # Overlay the priors
-    if with_priors:
-        amps = form_factor_fit.fits['full'].prior[amp_tag]
-        for idx, amp in enumerate(amps):
-            label = f'Prior: A{idx}'
-            plt.axhspan(ax, amp, label=label,
-                                alpha=0.25, color=colors[idx])
-    return ax
 
-def plot_amplitude_summary(ax, form_factor_fit, tag, osc=False, with_priors=True):
-    """ Make summary plot of the amplitudes"""
-    if osc:
-        amp_tag = f'{tag}:ao'
-        title = f'Partner Amplitudes: {tag}'
-    else:
-        amp_tag = f'{tag}:a'
-        title = f'Amplitudes: {tag}'
-        # Effective amplitude defined for decaying states only
-        ax = _plot_amp_eff(ax, form_factor_fit.ds[tag])
-
-    _plot_fit_amplitudes(ax, form_factor_fit, amp_tag, with_priors)
-    ax.set_title(title)
-    ax.set_xlabel("$t/a$")
-    ax.set_ylabel("Amplitude (lattice units)")
-    ax.legend(loc=1)
-    return ax
-
-def plot_states(form_factor_fit, axarr=None, osc=False, with_priors=True):
-    """Plots a 2x2 summary of the masses and amplitudes."""
-    if axarr is None:
-        fig, axarr = plt.subplots(nrows=2, ncols=2,
-                                        sharex=True, figsize=(20, 20))
-    ((ax1, ax2), (ax3, ax4)) = axarr
-    tags = form_factor_fit.ds._tags
-    # Masses in first row
-    for ax, tag in zip([ax1, ax2], tags):
-        _ = plot_energy_summary(ax, form_factor_fit, tag, osc=osc,
-                                        with_priors=with_priors)
-    # Amplitudes in second row
-    for ax, tag in zip([ax3, ax4], tags):
-        _ = plot_amplitude_summary(ax, form_factor_fit, tag, osc=osc,
-                                        with_priors=with_priors)
-    # Bands for fit range
-    ax_cols = [(ax1, ax3), (ax2, ax4)]
-    for tag, ax_col in zip(tags, ax_cols):
-        for ax in ax_col:
-            tmin = form_factor_fit.ds[tag].times.tmin
-            tmax = form_factor_fit.ds[tag].times.tmax
-            plt.axvline(ax, tmin, color='k', ls='--')
-            plt.axvline(ax, tmax, color='k', ls='--')
+def plot_states(form_factor_fit, nstates, a_fm=None):
+    """
+    Plots a grid summarizing the eneriges and amplitudes, comparing the prior
+    and posterior values along with the effective "mass" and amplitude. The 
+    energy is on the left, the amplitude is on the right.
+    """
+    fit = form_factor_fit.fits['full']
+    fig, axarr = plot_comparison(nstates, fit.prior, fit.p, a_fm)
+    def overlay(ax, corr, plot_func):
+        # grab limits *before* plotting on twin axis
+        ylim = ax.get_ylim()  
+        ax_twin = ax.twiny()
+        plot_func(ax_twin, corr)
+        # match to original ylim
+        ax.set_ylim(*ylim)
+        # shift to right side
+        _, xmax = ax_twin.get_xlim()
+        ax_twin.set_xlim(-xmax, xmax)
+        # sensible x-axis on the right side
+        ax_twin.set_xticks(np.arange(0, int(xmax), 5))
+        ax_twin.set_xticklabels(np.arange(0, int(xmax), 5))
+        ax_twin.legend(loc=1)
+        return ax_twin
+    (ax1, ax2), _, (ax3, ax4), _ = axarr
+    overlay(ax1, form_factor_fit.ds.c2_src, _plot_meff)
+    overlay(ax2, form_factor_fit.ds.c2_src, _plot_amp_eff)
+    overlay(ax3, form_factor_fit.ds.c2_snk, _plot_meff)
+    overlay(ax4, form_factor_fit.ds.c2_snk, _plot_amp_eff)
     fig.tight_layout()
-    return axarr
+    return fig, axarr
+
 
 def plot_form_factor(form_factor_fit, ax=None, tmax=None, color='k', prior=True):
     """
@@ -166,3 +110,80 @@ def plot_form_factor(form_factor_fit, ax=None, tmax=None, color='k', prior=True)
     ax.set_title("Form factor compared with estimates")
     ax.legend(loc=1)
     return ax
+
+
+Channel = namedtuple("Channel", ["prefix", "suffix", "title"])
+
+
+def get_channels(nstates):
+    """
+    Gets a list of "channels" to plot.
+    """
+    channels = []
+    if bool(nstates.n):
+        channels.append(Channel('light-light', '', 'Decaying Pion'))
+    if bool(nstates.no):
+        channels.append(Channel('light-light', 'o', 'Oscillating Pion'))
+    if bool(nstates.m):
+        channels.append(Channel('heavy-light', '', 'Decaying D-meson'))
+    if bool(nstates.mo):
+        channels.append(Channel('heavy-light', 'o', 'Oscillating D-meson'))
+    return channels
+
+
+def add_comparison(ax, prior, posterior, a_fm=None):
+    """
+    Adds a comparison plot of the prior vs the posterior to the given axis.
+    """
+    colors = sns.color_palette()
+    # Left side
+    for idx, value in enumerate(prior):
+        y = value
+        if a_fm is not None:
+            y = y * 197 / a_fm
+        plt.axhline(ax=ax, y=y, xmin=0.0, xmax=0.5,
+                    color=colors[idx], label=f"State {idx}")
+    # Right side
+    for idx, value in enumerate(posterior):
+        y = value
+        if a_fm is not None:
+            y = y * 197 / a_fm
+        plt.axhline(ax=ax, y=y, xmin=0.5, xmax=1.0,
+                          color=colors[idx])
+    # Formatting
+    ax.axvline(0.5, color='k')    
+    ax.set_xticks([0.25, 0.75])
+    ax.set_xticklabels(['Prior', 'Posterior'])
+    ax.legend(loc=2)
+
+
+def plot_comparison(nstates, prior, posterior, a_fm=None):    
+    """
+    Plots the prior vs the posterior values for the energies and amplitudes.
+    The resulting figure has two columns of plots, with energies on the left and
+    amplitudes on the right. In individual plots, the prior is on the left and
+    the posterior is on the right.
+    """
+    channels = get_channels(nstates)
+    nrows = len(channels)    
+    fig, axarr = plt.subplots(nrows=nrows, ncols=2, figsize=(20, 10*nrows))
+    
+    for channel, ax_row in zip(channels, axarr):
+        axL, axR = ax_row
+        key_dE = f'{channel.prefix}:dE{channel.suffix}'
+        key_amp = f'{channel.prefix}:a{channel.suffix}'            
+        # Left: Energy
+        energy_posterior = np.cumsum(posterior[key_dE])
+        energy_prior = np.cumsum(prior[key_dE])
+        add_comparison(axL, energy_prior, energy_posterior, a_fm)
+        # Right: Amplitude
+        amp_posterior =  posterior[key_amp] * np.sqrt(2.0 * energy_posterior) 
+        amp_prior = prior[key_amp] * np.sqrt(2.0 * energy_prior)
+        add_comparison(axR, amp_prior, amp_posterior)
+        # Formatting
+        axL.set_title(f"{channel.title}: Energies")
+        axR.set_title(f"{channel.title}: Amplitudes")
+        axL.set_ylabel("Energy (MeV)")
+        axR.set_ylabel(r"$A\times \sqrt{2 E}$ (lattice units)")
+
+    return fig, axarr
