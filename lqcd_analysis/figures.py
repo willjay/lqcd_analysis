de382e4a28e72e90dd625d7b1cd2eed3560240c5
William Jay <wjay@fnal.gov>
2020-07-20 16:09:06 -0500

    Fix bug, add support for meff in physical units

--- lqcd_analysis/figures.py
+++ lqcd_analysis/figures.py
@@ -27,13 +27,18 @@ def plot_form_factor_results(form_factor_fit, axarr=None):
     return fig, axarr
 
 
-def _plot_meff(ax, corr):
+def _plot_meff(ax, corr, a_fm=None):
     """
     Plots the effective mass and the FastFit guess for the given tag.
     """
-    ax = corr.plot_meff(ax=ax, avg=False, fmt='.', label='Effective mass')
-    ax = corr.plot_meff(ax=ax, avg=True, fmt='.',label='Smeared effective mass')
-    plt.axhline(ax, corr.fastfit.E, color='k', ls=':', label='FastFit guess')
+    ax = corr.plot_meff(ax=ax, avg=False, fmt='.', a_fm=a_fm,
+                        label='Effective mass')
+    ax = corr.plot_meff(ax=ax, avg=True, fmt='.', a_fm=a_fm,
+                        label='Smeared effective mass')
+    y = corr.fastfit.E
+    if a_fm is not None:
+        y = y * 197 / a_fm
+    plt.axhline(ax, y, color='k', ls=':', label='FastFit guess')
     return ax
 
 
@@ -67,11 +72,11 @@ def plot_states(form_factor_fit, nstates, a_fm=None):
     """
     fit = form_factor_fit.fits['full']
     fig, axarr = plot_comparison(nstates, fit.prior, fit.p, a_fm)
-    def overlay(ax, corr, plot_func):
+    def overlay(ax, corr, plot_func, **kwargs):
         # grab limits *before* plotting on twin axis
         ylim = ax.get_ylim()  
         ax_twin = ax.twiny()
-        plot_func(ax_twin, corr)
+        plot_func(ax_twin, corr, **kwargs)
         # match to original ylim
         ax.set_ylim(*ylim)
         # shift to right side
@@ -82,10 +87,17 @@ def plot_states(form_factor_fit, nstates, a_fm=None):
         ax_twin.set_xticklabels(np.arange(0, int(xmax), 5))
         ax_twin.legend(loc=1)
         return ax_twin
-    (ax1, ax2), _, (ax3, ax4), _ = axarr
-    overlay(ax1, form_factor_fit.ds.c2_src, _plot_meff)
+    channels = get_channels(nstates)
+    nrows = len(channels)
+    if nrows == 4:
+        (ax1, ax2), _, (ax3, ax4), _ = axarr
+    elif nrows == 3:
+        (ax1, ax2), (ax3, ax4), _ = axarr
+    else:
+        raise ValueError("Unrecognized number of channels?")
+    overlay(ax1, form_factor_fit.ds.c2_src, _plot_meff, a_fm=a_fm)
     overlay(ax2, form_factor_fit.ds.c2_src, _plot_amp_eff)
-    overlay(ax3, form_factor_fit.ds.c2_snk, _plot_meff)
+    overlay(ax3, form_factor_fit.ds.c2_snk, _plot_meff, a_fm=a_fm)
     overlay(ax4, form_factor_fit.ds.c2_snk, _plot_amp_eff)
     fig.tight_layout()
     return fig, axarr
