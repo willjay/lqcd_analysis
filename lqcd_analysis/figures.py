e8b9fb2d9a0267ce892ce82f13003dc283f76eeb
William Jay <wjay@fnal.gov>
2021-03-10 15:29:07 -0600

    Add function for viewing relativistic dispersion

--- lqcd_analysis/figures.py
+++ lqcd_analysis/figures.py
@@ -1,5 +1,11 @@
-import numpy as np
+"""
+Visualization routines.
+In principle, this module should contain visualization routines containing physics or analysis
+logic.
+"""
 from collections import namedtuple
+import numpy as np
+import gvar as gv
 import seaborn as sns
 from . import visualize as plt
 
@@ -77,14 +83,14 @@ def _plot_amp_eff(ax, corr):
 def plot_states(form_factor_fit, nstates, a_fm=None):
     """
     Plots a grid summarizing the eneriges and amplitudes, comparing the prior
-    and posterior values along with the effective "mass" and amplitude. The 
+    and posterior values along with the effective "mass" and amplitude. The
     energy is on the left, the amplitude is on the right.
     """
     fit = form_factor_fit.fits['full']
     fig, axarr = plot_comparison(nstates, fit.prior, fit.p, a_fm)
     def overlay(ax, corr, plot_func, **kwargs):
         # grab limits *before* plotting on twin axis
-        ylim = ax.get_ylim()  
+        ylim = ax.get_ylim()
         ax_twin = ax.twiny()
         plot_func(ax_twin, corr, **kwargs)
         # match to original ylim
@@ -125,10 +131,10 @@ def plot_form_factor(form_factor_fit, ax=None, tmax=None, color='k', prior=True)
     ax = form_factor_fit.ds.plot_ratio(ax=ax, tmax=tmax)
     # Plot the prior value for the plateau in "R"
     plt.axhspan(ax, y=form_factor_fit.r_prior * norm,
-                    alpha=0.25, color=color, label='Prior: R')
+                alpha=0.25, color=color, label='Prior: R')
     # Plot the fit value for the plateau in "R"
     plt.axhline(ax, y=form_factor_fit.r * norm,
-                        alpha=0.50, color=color, label='Fit: R')
+                alpha=0.50, color=color, label='Fit: R')
     ax.set_title("Form factor compared with estimates")
     ax.legend(loc=1)
     return ax
@@ -171,15 +177,15 @@ def add_comparison(ax, prior, posterior, a_fm=None):
         if a_fm is not None:
             y = y * 197 / a_fm
         plt.axhline(ax=ax, y=y, xmin=0.5, xmax=1.0,
-                          color=colors[idx])
+                    color=colors[idx])
     # Formatting
-    ax.axvline(0.5, color='k')    
+    ax.axvline(0.5, color='k')
     ax.set_xticks([0.25, 0.75])
     ax.set_xticklabels(['Prior', 'Posterior'])
     ax.legend(loc=2)
 
 
-def plot_comparison(nstates, prior, posterior, a_fm=None):    
+def plot_comparison(nstates, prior, posterior, a_fm=None):
     """
     Plots the prior vs the posterior values for the energies and amplitudes.
     The resulting figure has two columns of plots, with energies on the left and
@@ -187,19 +193,19 @@ def plot_comparison(nstates, prior, posterior, a_fm=None):
     the posterior is on the right.
     """
     channels = get_channels(nstates)
-    nrows = len(channels)    
+    nrows = len(channels)
     fig, axarr = plt.subplots(nrows=nrows, ncols=2, figsize=(20, 10*nrows))
-    
+
     for channel, ax_row in zip(channels, axarr):
         axL, axR = ax_row
         key_dE = f'{channel.prefix}:dE{channel.suffix}'
-        key_amp = f'{channel.prefix}:a{channel.suffix}'            
+        key_amp = f'{channel.prefix}:a{channel.suffix}'
         # Left: Energy
         energy_posterior = np.cumsum(posterior[key_dE])
         energy_prior = np.cumsum(prior[key_dE])
         add_comparison(axL, energy_prior, energy_posterior, a_fm)
         # Right: Amplitude
-        amp_posterior =  posterior[key_amp] * np.sqrt(2.0 * energy_posterior) 
+        amp_posterior = posterior[key_amp] * np.sqrt(2.0 * energy_posterior)
         amp_prior = prior[key_amp] * np.sqrt(2.0 * energy_prior)
         add_comparison(axR, amp_prior, amp_posterior)
         # Formatting
@@ -209,3 +215,62 @@ def plot_comparison(nstates, prior, posterior, a_fm=None):
         axR.set_ylabel(r"$A\times \sqrt{2 E}$ (lattice units)")
 
     return fig, axarr
+
+
+def relativistic_dispersion(dataframe, ax=None, x_offset=0, alpha_v=None, **kwargs):
+    """
+    Plots the quantity E^2 / (M^2 + p^2) as a test of the relativistic dispersion relation.
+    Also optionally plots the envelope of expected discretization errors of size O(alpha*(ap)^2).
+    Args:
+        dataframe: pandas.DataFrame with columns 'energy', 'p2', 'phat2'
+        ax: plot axis. Default is None
+        x_offset: float, amount by which to offset the horizontal axis. Default is 0.
+        alpha_v: float or gvar, the value of the strong coupling constant alpha_V(a/2)
+        **kwargs: keyword arguments passed to the function errorbar
+    Returns:
+        ax: plot axis
+    """
+    for col in ['energy', 'p2', 'phat2']:
+        if col not in dataframe.columns:
+            raise ValueError("Missing column. Looking for '{col}' but found {dataframe.columns}")
+
+    if ax is None:
+        _, ax = plt.subplots(1, figsize=(5, 5))
+
+    dataframe = dataframe.sort_values(by='phat2')
+    # Isolate zero-momentum data
+    at_rest = (dataframe['phat2'] == 0)
+    if dataframe[at_rest].empty:
+        raise ValueError("Missing zero-momentum data.")
+    if len(dataframe[at_rest]) > 1:
+        raise ValueError("Non-unique zero-momentum data.")
+    at_rest_idx = dataframe[at_rest].index.item()
+
+    # Plot E^2 / (M^2 + p^2) versus phat2
+    M2 = dataframe['energy'][at_rest_idx]**2.0
+    E2 = dataframe['energy']**2.0
+    p2 = dataframe['p2'].values
+    y = E2 / (M2 + p2)
+    x = dataframe['phat2'] + x_offset
+    kwargs['markeredgewidth'] = kwargs.get('markeredgewidth', 2)
+    kwargs['capsize'] = kwargs.get('capsize', 5)
+    kwargs['fmt'] = kwargs.get('fmt', 'o')
+    ax = plt.errorbar(ax, x, y, **kwargs)
+
+    # Plot envelope of expected discretization errors 1 +/- alpha (ap)^2
+    if alpha_v is not None:
+        x = np.linspace(0, max(x))
+        y = 1.0 + gv.mean(alpha_v) * np.linspace(0, max(p2))
+        plt.errorbar(ax, x, y, color='k', fmt='--')
+        y = 1.0 - gv.mean(alpha_v) * np.linspace(0, max(p2))
+        ax = plt.errorbar(ax, x, y, color='k', fmt='--', label=r"$1 \pm \alpha_V(a/2) a^2 p^2$")
+
+    # Formatting
+    ax.set_xlabel(r"$\hat{p}^2$")
+    ax.set_ylabel(r"$E^2/(p^2 + m^2)$")
+    xticks = dataframe['phat2'].values
+    ax.set_xticks(xticks)
+    ax.set_xticklabels([f"{xtick:.0f}" for xtick in xticks])
+
+
+    return ax
