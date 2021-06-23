5b2eb4c6dc6db86cd5be7b9e85b59eaa025750bb
William Jay <wjay@fnal.gov>
2021-06-23 14:09:38 -0500

    Fix typo

--- lqcd_analysis/resample.py
+++ lqcd_analysis/resample.py
@@ -12,27 +12,27 @@ class Bootstrap:
             nensemble: int total number of bootstrap ensembles
         Returns:
             Bootstrap object
-            
+
         Notes:
         "nconfigs" is the number of configurations in the original dataset.
         Following the notation of DeGrand and DeTar Sec 9.2.3, "nconfigs = N".
-        
+
         "nresample" denotes the number of times to sample from the original
         data to produce a single bootstrap ensemble. Following the notation
         of DeGrand and DeTar Sec 9.2.3, "nresample = M".
-        
-        "nensemble" denotes the total number of bootstrap ensembles to 
+
+        "nensemble" denotes the total number of bootstrap ensembles to
         consider. Following the notation of DeGrand and DeTar Sec. 9.2.3,
         "nensemble = P"
         """
         self.data = data
         self.is_dict = self._is_dict()
-        nconfigs = self._nconfigs()      
+        nconfigs = self._nconfigs()
         if nresample is None:
             nresample = nconfigs
         if nensemble is None:
             nensemble = 4 * nconfigs
-        np.random.seed(seed)            
+        np.random.seed(seed)
         self.seed = seed
         self.nconfigs = nconfigs
         self.nresample = nresample
@@ -43,7 +43,7 @@ class Bootstrap:
             size=(nensemble, nresample)
         )
         self.checksums = self._checksums()
-        
+
     @property
     def info(self):
         return {
@@ -52,7 +52,7 @@ class Bootstrap:
             'nresample': self.nresample,
             'nensemble': self.nensemble,
         }
-        
+
     def _is_dict(self):
         attrs = ['keys','values','items']
         return np.all([hasattr(self.data, attr) for attr in attrs])
@@ -66,13 +66,13 @@ class Bootstrap:
     def _sample(self, draw):
         if self.is_dict:
             return {key: value[draw, :] for key, value in self.data.items()}
-        return data[draw, :]
+        return self.data[draw, :]
 
     def _checksums(self):
         def compute_checksum(arr):
             return hashlib.sha256(arr.tobytes()).hexdigest()
         return np.apply_along_axis(compute_checksum, axis=1, arr=self.draws)
-    
+
     def __iter__(self):
         for checksum, draw in zip(self.checksums, self.draws):
             yield checksum, self._sample(draw)
@@ -84,5 +84,4 @@ class Bootstrap:
     def sha256(self, draw):
         """Computes the SHA256 checksum for the draw."""
         return hashlib.sha256(draw.tobytes()).hexdigest()
-    
-    
+
